c55cfd4de18fd9407e6be27c6bfe1ee7097f5871
jokoolle-183 <jovan.stanisic18@gmail.com>
2020-05-02 01:40:29 +0200

    Added navigation component

--- app/src/main/java/com/example/taskreminderapp/MainActivity.kt
+++ app/src/main/java/com/example/taskreminderapp/MainActivity.kt
@@ -1,19 +1,13 @@
 package com.example.taskreminderapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.taskreminderapp.ui.LogFragment
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        supportFragmentManager.beginTransaction()
-            .add(R.id.fragmentContainer, LogFragment())
-            .addToBackStack(null)
-            .commit()
     }
+
 }
--- app/src/main/java/com/example/taskreminderapp/ui/LogListFragment.kt
+++ app/src/main/java/com/example/taskreminderapp/ui/LogListFragment.kt
@@ -5,15 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.taskreminderapp.R
 import com.example.taskreminderapp.data.model.LogEntryModel
 import com.example.taskreminderapp.data.model.LogType
 import com.example.taskreminderapp.ui.adapter.LogListAdapter
-import kotlinx.android.synthetic.main.fragment_log.view.*
+import kotlinx.android.synthetic.main.fragment_log_list.view.*
 
-class LogFragment : Fragment() {
+class LogListFragment : Fragment() {
 
     private lateinit var rvLogList: RecyclerView
     private lateinit var logListAdapter: LogListAdapter
@@ -23,7 +24,7 @@ class LogFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_log, container, false).apply {
+        return inflater.inflate(R.layout.fragment_log_list, container, false).apply {
             logListAdapter = LogListAdapter()
 
             rvLogList = recyclerView
@@ -31,14 +32,17 @@ class LogFragment : Fragment() {
             rvLogList.layoutManager = LinearLayoutManager(context)
 
             populateAdapter()
+            fabAddTask.setOnClickListener {
+                findNavController().navigate(R.id.action_logListFragment_to_logDetailFragment)
+            }
         }
     }
 
     private fun populateAdapter() {
-        val list = arrayListOf<LogEntryModel>(
-            LogEntryModel(title = "First task", type = LogType.TASK),
-            LogEntryModel(title = "Second task", type = LogType.TASK),
-            LogEntryModel(title = "Event",type = LogType.EVENT)
+        val list = arrayListOf(
+            LogEntryModel(title = "Cestitaj Mikici rodjendan", type = LogType.TASK),
+            LogEntryModel(title = "Reci da je volis", type = LogType.TASK),
+            LogEntryModel(title = "Ljubaaav", type = LogType.EVENT)
         )
         logListAdapter.loadList(list)
     }
